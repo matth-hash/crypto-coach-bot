bb8ec067e45a0270985fd710f902968db31cfaf7
matth-hash <matthieucollon@gmail.com>
2026-03-11 18:47:14 +0100

    Add lexique_handlers router to main bot

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-
 from config import TELEGRAM_TOKEN
 from bot.handlers import router as main_router
 from bot.exchange_handlers import router as exchange_router
@@ -11,24 +10,26 @@ from bot.gamification_handlers import router as gamification_router
 from bot.payment_handlers import router as payment_router
 from bot.help_handlers import router as help_router
 from bot.callback_handlers import router as callback_router
+from bot.lexique_handlers import router as lexique_router
 from database import init_db
 
 logging.basicConfig(level=logging.INFO)
 
 async def main():
     await init_db()
-    
+
     bot = Bot(token=TELEGRAM_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
-    
+
     dp.include_router(exchange_router)
     dp.include_router(psychology_router)
     dp.include_router(gamification_router)
     dp.include_router(payment_router)
     dp.include_router(help_router)
+    dp.include_router(lexique_router)
     dp.include_router(callback_router)
     dp.include_router(main_router)
-    
+
     print("🚀 CryptoCoach Bot démarré !")
     await dp.start_polling(bot)
 
